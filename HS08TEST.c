638c271a8b76319dd1b1b00bb2991ee3bb587b7a
Amit Bansal <amitbansal@Amits-MacBook-Pro.local>
2016-09-12 12:41:41 +0530

    solved

--- HS08TEST.c
+++ HS08TEST.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 
-// wrong answer
+// working
 //codechef username amitbansal7
 
 int main()
 {
     float b,n,i;
-    int c,x;
+    int c,w;
+    
 
-    scanf("%d%f",&x,&b);
-    c=x;
-    i=x+0.50;
-  if(x<=b){
-       if(c%5==0)
-       {
-        n=b-x-0.50;
-        printf("%.2f\n",n);
-       }
-       else printf("%.2f\n",b);
+    scanf("%d",&w);
+    scanf("%f",&b);
+    printf("\n");
+
+    i=w+0.50;
+
+    if(w%5==0 && i<=b)
+    {
+    	printf("%.2f",b-i);
+
     }
-    else printf("%.2f\n",b);
-    return 0;
+
+    else printf("%.2f",b);
+   
+   return 0;
+
 
 
 }

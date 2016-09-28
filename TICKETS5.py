457b6d03f87189da47f6bb17d4b7a065c28ba16a
Amit Bansal <amitbansal@Amit.local>
2016-09-28 22:04:15 +0530

    error

--- TICKETS5.py
+++ TICKETS5.py
@@ -1,38 +1,50 @@
-# not working 
 # codechef - amitbansal7
-t = int(input())
+# not working if string has more the 6 letters..
+ 
+def tickets(t):
 
-for i in range(t):
+  
+ 
+  for i in range(t):
     f=0
     ticket = str(input())
     A = list(ticket)
-
+ 
     length =len(list(ticket))
     length = int(length)
     count = length/2
     count = int(count)
     
-
+ 
     if(length%2==0):
-
+ 
       if length==2 and A[0]!=A[1]:
           print('YES')
-
+ 
       else:
-        for j in range(2,count):
           
-
-          if A[j] == A[j+1]:
+        f =1
+        for j in range(0,count,2):
+          
+ 
+          if A[j] == A[j+2]:
             f = f+1;
-
-        if f == count/2:
+ 
+        if f == count:
           print('YES')
-
+ 
         else:
           print('NO')
-
-
+ 
+ 
     else:
         print('NO')
-
-
+        
+        
+  return 0    
+        
+t = int(input())
+ 
+if 1 <= t<= 100:
+   tickets(t)        
+ 

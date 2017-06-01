db58f09dcbd6a03ef12fa33d539db29310cdda0a
Amit Bansal <007amitbansal@gmail.com>
2017-06-01 14:40:05 +0530

    HUBULLU - Hubulullu

---
+++ SPOJ/HUBULLU.cpp
@@ -0,0 +1,29 @@
+/* Amit Bansal - @amitbansal7 */
+#include <bits/stdc++.h>
+#include <string>
+#define lli long long int
+#define llu unsigned long long int
+#define S(x) scanf("%d",&x)
+#define Sl(x) scanf("%lld",&x)
+#define Mset(p,i) memset(p,i,sizeof(p))
+#define mlc(t,n) (t *)malloc(sizeof(t)*n)
+#define NIL -1
+#define INF 0x3f3f3f3f
+#define TC int testcase; S(testcase); while(testcase--)
+#define Pi 3.14159
+using namespace std;
+
+int main()
+{
+	TC
+	{
+		int a,b;
+		S(a);S(b);
+
+		if(b==0)
+			printf("Airborne wins.\n");
+
+		else
+			printf("Pagfloyd wins.\n");
+	}
+}
